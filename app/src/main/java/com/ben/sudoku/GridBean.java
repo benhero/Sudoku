fc2a7eea25dd9a38bcf9813057d63424690506a7
Benhero <benhero@126.com>
2018-07-16 20:57:59 +0800

    Model： 新增格子数据Bean

---
+++ app/src/main/java/com/ben/sudoku/GridBean.java
@@ -0,0 +1,75 @@
+package com.ben.sudoku;
+
+import java.util.Arrays;
+
+/**
+ * 单个格子的数据Bean
+ *
+ * @author chenbenbin
+ * @date 2018/7/16
+ */
+public class GridBean {
+    /**
+     * 原始值
+     */
+    private int mSource;
+    /**
+     * 填入的值
+     */
+    private int mInput;
+    /**
+     * 提示数字
+     */
+    private int[] mTips = new int[9];
+    /**
+     * 提示数字个数
+     */
+    private int mTipsCount = 0;
+
+    public GridBean() {
+        Arrays.fill(mTips, 0);
+    }
+
+    public int getSource() {
+        return mSource;
+    }
+
+    public void setSource(int source) {
+        mSource = source;
+    }
+
+    public int getInput() {
+        return mInput;
+    }
+
+    public void setInput(int input) {
+        mInput = input;
+    }
+
+    public int[] getTips() {
+        return mTips;
+    }
+
+    public void setTips(int[] tips) {
+        mTips = tips;
+    }
+
+    public void setTip(int num, boolean show) {
+        mTips[num - 1] = show ? num : 0;
+    }
+
+    public int getTip(int index) {
+        return mTips[index];
+    }
+
+    public int getTipsCount() {
+        return mTipsCount;
+    }
+
+    public void updateTipsCount() {
+        mTipsCount = 0;
+        for (int tip : mTips) {
+            mTipsCount += tip == 0 ? 0 : 1;
+        }
+    }
+}
